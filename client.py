8424c2d407df8ad0752667232b6ac10af6900e77
Alejandro Zambrano <alejandro_zambrano_@hotmail.com>
2025-06-02 17:05:23 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 #url='http://localhost:8000'
-url='http: api-students-rcw-h2gchucugmayhph6.canadaeast-01.azurewebsites.net'
+url='https://api-students-rcw-h2gchucugmayhph6.canadaeast-01.azurewebsites.net'
 response=requests.get(url)
 response=response.json()
 print(response['message'])
