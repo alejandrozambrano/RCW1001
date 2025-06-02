ea2e4f1a858aa2f62acc006bd67c4473cd1acc3c
Alejandro Zambrano <alejandro_zambrano_@hotmail.com>
2025-06-02 17:33:32 -0400

    xd
    s

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-#url='http://localhost:8000'
+# url='http://localhost:8000'
 url='https://api-students-rcw-h2gchucugmayhph6.canadaeast-01.azurewebsites.net/'
 response=requests.get(url)
 response=response.json()
